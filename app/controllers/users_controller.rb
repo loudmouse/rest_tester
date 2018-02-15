db63c9f5a2bd6299e4eab8d4d8618049b52e84a1
Nolan Neeley <nolanneeley@gmail.com>
2018-02-15 05:57:27 -0600

    rest tester commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def index
+     @name
+  end
+
+  def show
+    @name
+  end
+
+  def new
+    @name
+  end
+
+  def edit
+    @name
+  end
+
+  def create
+    @name 
+  end
+end
